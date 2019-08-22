2216fb6e3ae4be25bd76760b1f3667f7da49339d
GutZuFusss <leon.ikinger@googlemail.com>
2019-08-22 15:25:33 +0200

    fixed some unwanted tabulators

--- src/main/java/SQLWrapper.java
+++ src/main/java/SQLWrapper.java
@@ -8,38 +8,38 @@ import java.sql.Statement;
 public class SQLWrapper {
 	private static final String DB_PATH = "db/img_finder_data.db";
 	private static final int QUERY_TIMEOUT = 30; 
-	
+
 	private static Connection connection = null;
 	private static Statement statement = null;
 
 	// constructor is private to prevent instantiation because all methods in this class will be static anyways
 	private SQLWrapper() { }
-	
+
 	public static void execSQL(String sql) throws ClassNotFoundException, SQLException {
 		createConStateIfNeeded();
 		statement.executeUpdate(sql);
 	}
-	
+
 	public static ResultSet execSQLFetch(String query) throws ClassNotFoundException, SQLException {
 		createConStateIfNeeded();
 		ResultSet resultOfQuery = statement.executeQuery(query);
 
 		return resultOfQuery;
 	}
-	
+
 	// START_MISC_FUNCTIONS
 	private static Connection createConnection() throws ClassNotFoundException, SQLException {
 		Class.forName("org.sqlite.JDBC");
 		return DriverManager.getConnection("jdbc:sqlite:" + DB_PATH);
 	}
-	
+
 	private static Statement createStatement(Connection connection) throws SQLException {
 		Statement statement = connection.createStatement();
 		statement.setQueryTimeout(QUERY_TIMEOUT);
 
 		return statement;
 	}
-	
+
 	private static void createConStateIfNeeded() {
 		// check wether our connection & statement are active already
 		if(isConnectionOpened()) {
@@ -57,7 +57,7 @@ public class SQLWrapper {
 			}
 		}
 	}
-	
+
 	private static boolean isConnectionOpened() { return connection == null; }
 
 	private static boolean isStatementOpened() { return statement == null; }
