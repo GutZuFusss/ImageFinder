e378dfbd9438d8778517416b6748e3896d9a122c
GutZuFusss <leon.ikinger@googlemail.com>
2019-08-22 14:40:54 +0200

    moved p.s.v. main() all the way to the bottom

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,11 +9,11 @@ public class Main {
 		//ocrWrapper.scanDirectory("test_images");
 	}
 
-	public static void main(String[] args) {
-		new Main();
-	}
-
 	public OCRWrapper getOCR() { return ocrWrapper; }
 	
 	public ImageManipulation getIMGManipulator() { return imgManipulator; }
+	
+	public static void main(String[] args) { // entry point
+		new Main();
+	}
 }
