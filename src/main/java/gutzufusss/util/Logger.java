a44d85bc03c537404c7f02a97c352ee404da725f
GutZuFusss <leon.ikinger@googlemail.com>
2019-08-22 18:04:50 +0200

    almost ready for live tests

--- src/main/java/gutzufusss/util/Logger.java
+++ src/main/java/gutzufusss/util/Logger.java
@@ -6,7 +6,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Logger {
-	private final String LOGS_PATH = "logs/{TS}.log".replaceFirst("{TS}", getTimestamp(true));
+	private final String LOG_PATH = "logs/{TS}.log".replaceFirst("{TS}", getTimestamp(true));
 
 	public static final int LVL_INFO = 1;
 	public static final int LVL_WARN = 2;
@@ -14,13 +14,22 @@ public class Logger {
 	public static final int LVL_FATAL = 8;
 	public static final int LVL_DEBUG = 16;
 
+	private File logFile;
+
 	public Logger() {
-		File log = new File(LOGS_PATH);
+		logFile = new File(LOG_PATH);
 		try {
-			if(!log.getParentFile().exists())
-				log.getParentFile().createNewFile();
+			if(!logFile.getParentFile().exists())
+				logFile.getParentFile().createNewFile();
 		} catch(IOException e) {
 			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// now it is safe to create the current's session logfile
+		try {
+			logFile.createNewFile();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
@@ -30,14 +39,13 @@ public class Logger {
 				+ (printCallerMethod ? "::" + Thread.currentThread().getStackTrace()[1].getMethodName() : "");
 
 	}
-	
+
 	private String getTimestamp(boolean logger) {
 		if(logger)
 			return LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH:mm:ss"));
 		else
 			return LocalDate.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
 	}
-	
-	private String getTimestamp() { return getTimestamp(false); }
 
+	private String getTimestamp() { return getTimestamp(false); }
 }
