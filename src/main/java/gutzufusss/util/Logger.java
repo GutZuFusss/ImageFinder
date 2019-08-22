939ff9d76da1d0a19cff5583c6bf688122894814
GutZuFusss <leon.ikinger@googlemail.com>
2019-08-22 17:22:05 +0200

    added some output levels

--- src/main/java/gutzufusss/util/Logger.java
+++ src/main/java/gutzufusss/util/Logger.java
@@ -1,7 +1,12 @@
 package gutzufusss.util;
 
 public class Logger {
-
+	public static final int LVL_DEV = 0;
+	public static final int LVL_INFO = 1;
+	public static final int LVL_WARN = 2;
+	public static final int LVL_ERROR = 4;
+	public static final int LVL_FATAL = 8;
+	
 	public Logger() {
 		
 	}
