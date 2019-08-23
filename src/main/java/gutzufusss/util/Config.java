876f9f0b94c2bc9ab05d20b8eaf861fefe9b8b64
GutZuFusss <leon.ikinger@googlemail.com>
2019-08-23 13:10:35 +0200

    more convinient way to handle default config

--- src/main/java/gutzufusss/util/Config.java
+++ src/main/java/gutzufusss/util/Config.java
@@ -13,6 +13,8 @@ public class Config {
 	public ConfigVariables curConf = new ConfigVariables();
 	/* config variables end here */
 	
+	public ConfigVariables defConf = new ConfigVariables();
+	
 	private Logger logger;
 	private ConfigDBController configDB;
 
@@ -25,20 +27,16 @@ public class Config {
 	
 	public ConfigDBController getConfigDB() { return configDB; }
 	
-	public final class DefaultConfig {
-		public final String			configName	= "default_config";
-		public final boolean		debug		= false;
-		public final int			logLevel	= Logger.LVL_INFO;
-		public final int			critConf	= 55;
+	public void setDefaultConf (){
+		defConf.configName		= "default_config";
+		defConf.debug			= false;
+		defConf.logLevel		= Logger.LVL_INFO;
+		defConf.critConf		= 55;
 	}
-	
-	public DefaultConfig defConf = new DefaultConfig();
-	
+
 	private void initConfigVars() {
-		// TODO: call function in configDB that attempts to load the config
-		curConf.configName =		"default_config";
-		curConf.debug =				defConf.debug;
-		curConf.logLevel =			defConf.logLevel;
-		curConf.critConf =			defConf.critConf;
+		// TODO: call function in configDB that attempts to load the config or else just set curConf = defConf
+		
+		curConf = defConf;
 	}
 }
