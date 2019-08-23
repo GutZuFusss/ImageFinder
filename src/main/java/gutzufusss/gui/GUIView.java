8fb36eb13cba8d64d17ac97c43489404fbfc9019
GutZuFusss <leon.ikinger@googlemail.com>
2019-08-23 16:24:12 +0200

    hella sick gui

--- src/main/java/gutzufusss/gui/GUIView.java
+++ src/main/java/gutzufusss/gui/GUIView.java
@@ -1,31 +1,81 @@
 package gutzufusss.gui;
 
-import java.awt.BorderLayout;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JTextField;
+import javax.swing.LookAndFeel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import org.pushingpixels.substance.api.SubstanceLookAndFeel;
+import org.pushingpixels.substance.api.skin.GraphiteAquaSkin;
+import org.pushingpixels.substance.api.skin.GraphiteSkin;
+import org.pushingpixels.substance.api.skin.SubstanceGraphiteAquaLookAndFeel;
+import org.pushingpixels.substance.api.skin.SubstanceGraphiteGlassLookAndFeel;
+import org.pushingpixels.substance.api.skin.SubstanceGraphiteLookAndFeel;
+
+import com.alee.laf.WebLookAndFeel;
+
+import javax.swing.JScrollBar;
+import java.awt.Dimension;
+import java.awt.Color;
 
 @SuppressWarnings("serial")
 public class GUIView extends JFrame {
-	private JLabel testLabel; 
-	private JButton testButton;
 
 	public GUIView() {
+		setUpLookAndFeel();
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("ImageFinder");
+		setResizable(false);
+		setSize(new Dimension(980, 530));
+		getContentPane().setSize(new Dimension(980, 530));
+		getContentPane().setLayout(null);
+
+		JScrollBar scrollBar = new JScrollBar();
+		scrollBar.setBounds(931, 260, 17, 215);
+		getContentPane().add(scrollBar);
+
+		JButton btnBrowse = new JButton("Browse...");
+		btnBrowse.setBounds(290, 62, 95, 25);
+		getContentPane().add(btnBrowse);
+
+		JButton btnStartScanning = new JButton("Start scanning");
+		btnStartScanning.setBounds(9, 62, 145, 25);
+		getContentPane().add(btnStartScanning);
+
+		JLabel lblTypeThePath = new JLabel("Type a path or click the \"Browse...\" button below.");
+		lblTypeThePath.setAutoscrolls(true);
+		lblTypeThePath.setBounds(12, 0, 456, 30);
+		getContentPane().add(lblTypeThePath);
+
+		JTextField textField = new JTextField();
+		textField.setBounds(9, 31, 376, 22);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+
+		JList<String> list = new JList<String>();
+		list.setBounds(17, 260, 931, 215);
+		getContentPane().add(list);
+
 		initializeGUI();
 		this.setVisible(true);
 	}
-	
+
 	private void initializeGUI() {
-		this.setBounds(900, 500, 700, 500);
-		testLabel = new JLabel("SCHNAPS NUR AUF EIS, NUR VON BLUNTS WERD ICH BREIT");
-		testLabel.setHorizontalAlignment(JLabel.CENTER);
-        testButton = new JButton("Heyaaa");
-        testButton.addActionListener(new GUIController(this));
-        this.add(testLabel, BorderLayout.NORTH);
-        this.add(testButton, BorderLayout.CENTER);
-        this.pack();
-        this.setTitle("MVC");
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	private void setUpLookAndFeel() {
+		getContentPane().setBackground(Color.DARK_GRAY);
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		try {
+			UIManager.setLookAndFeel(new SubstanceGraphiteLookAndFeel());
+		} catch (Exception e) {
+			System.out.println("Substance Graphite failed to initialize");
+		}
 	}
 }
